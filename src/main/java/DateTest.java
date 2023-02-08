20d4d4c5bdbef3c1daa19ce8f4bf281d5f80b6f7
ramlifting <ramlifting@137.125.154.75>
2023-02-08 14:48:13 -0500

    Module 2 Question 2

---
+++ src/main/java/DateTest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ramlifting
+ */
+public class DateTest {
+    public static void main(String[] args) {
+        Date date = new Date(2, 5, 2023);
+        System.out.print("Date: ");
+        date.displayDate();
+    }
+}
+
